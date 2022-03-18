b1528f3d076604932c24e66d94bb1054fef8d637
hodeng28 <ghepd28@gmail.com>
2022-03-18 11:14:58 +0900

    feat: movie api

--- src/Components/NowPlaying.js
+++ src/Components/NowPlaying.js
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import moviesApi from "../Api/MoviesApi";
+
 const NowPlaying = () => {
-  return <div>현재 상영작</div>;
+  const [movieData, setMovieData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchPopular = async () => {
+    setLoading(true);
+    try {
+      const response = await moviesApi.getNowPlaying();
+      setMovieData(response.data.results);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  if (loading) return <div>로딩</div>;
+  return (
+    <>
+      <ul>
+        {movieData &&
+          movieData.map((movie) => {
+            return (
+              <li key={movie.id}>
+                <img
+                  alt={movie.original_title}
+                  src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                />
+              </li>
+            );
+          })}
+      </ul>
+    </>
+  );
 };
 
 export default NowPlaying;
--- src/Components/Popular.js
+++ src/Components/Popular.js
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+import moviesApi from "../Api/MoviesApi";
+
 const Popular = () => {
-  return <div>인기 상영작 페이지</div>;
+  const [movieData, setMovieData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchPopular = async () => {
+    setLoading(true);
+    try {
+      const response = await moviesApi.getPopular();
+      setMovieData(response.results);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  if (loading) return <div>로딩</div>;
+
+  return (
+    <>
+      <ul>
+        {movieData &&
+          movieData.map((movie) => {
+            return (
+              <li key={movie.id}>
+                <img
+                  alt={movie.original_title}
+                  src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                />
+              </li>
+            );
+          })}
+        ;
+      </ul>
+    </>
+  );
 };
 
 export default Popular;
