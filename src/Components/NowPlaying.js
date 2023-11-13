94a639df98abb16f867d38a74b32b7cf7410c985
hodeng28 <ghepd28@gmail.com>
2023-11-13 16:10:27 +0900

    fix: useEffect dependency

--- src/Components/NowPlaying.js
+++ src/Components/NowPlaying.js
@@ -9,7 +9,7 @@ const NowPlaying = () => {
 
   useEffect(() => {
     fetchNowplaying();
-  }, [fetchNowplaying]);
+  }, []);
 
   return (
     <>
--- src/Components/Popular.js
+++ src/Components/Popular.js
@@ -9,7 +9,7 @@ const Popular = () => {
 
   useEffect(() => {
     fetchPopular();
-  }, [fetchPopular]);
+  }, []);
 
   return (
     <>
